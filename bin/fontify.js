5a729ee688b50ea65463a47ccb791eb1ed39b5f5
Jacob Ward <jacob@clearwateranalytics.com>
2016-11-07 16:52:53 -0700

    modifying to work with multiple destination directories

--- bin/fontify.js
+++ bin/fontify.js
@@ -9,26 +9,23 @@ var appRoot = require('app-root-dir').get();
 var dataFile = path.join(appRoot, 'fontify.json');
 
 if (!isThere(dataFile)) {
-  console.error('Configuration file not found: fontify.json');
-  return;
+	console.error('Configuration file not found: fontify.json');
+	return;
 }
 
 var data = require(dataFile);
 
-fontify.dest = data.dest || '.';
+data.forEach(function (entry) {
+	entry.dest = entry.dest || '.';
 
-if (!isThere(path.join(appRoot, fontify.dest))) {
-  console.error('Destination folder not found: ' + fontify.dest);
-  return;
-}
+	entry.modules.forEach(function (module) {
+		fontify.copy(module, entry.dest, function (err) {
+			if (err) {
+				console.error(err);
+				return;
+			}
 
-data.modules.forEach(function(module) {
-  fontify.copy(module, function(err) {
-    if (err) {
-      console.error(err);
-      return;
-    }
-
-    console.log("Finished copying " + module);
-  });
+			console.log("Finished copying " + module);
+		});
+	});
 });
--- index.js
+++ index.js
@@ -1,42 +1,41 @@
 var findit = require('findit');
 var path = require('path');
-var stream = require('stream');
 var fs = require('fs');
 var isThere = require('is-there');
 var appRoot = require('app-root-dir').get();
 
 module.exports = {
-  dest: '.',
-  copy: function(moduleName, callback) {
-    var _this = this;
-    var modulePath = path.join(appRoot, 'node_modules', moduleName);
+	copy: function (moduleName, dest, callback) {
+		var modulePath = path.join(appRoot, 'node_modules', moduleName);
 
-    if (!isThere(modulePath)) {
-      callback("Module not found: " + moduleName);
-      return;
-    }
+		if (!isThere(modulePath)) {
+			callback("Module not found: " + moduleName);
+			return;
+		}
 
-    if (!isThere(path.join(appRoot, this.dest))) {
-      callback('Destination folder not found: ' + this.dest);
-      return;
-    }
+		var finder = findit(modulePath);
 
-    var finder = findit(modulePath);
+		if (!isThere(path.join(appRoot, dest)))
+			fs.mkdirSync(path.join(appRoot, dest), '0776');
 
-    if (!isThere(path.join(appRoot, this.dest, 'fonts')))
-      fs.mkdirSync(path.join(appRoot, this.dest, 'fonts'), '0776');
+		finder.on('file', function (file, stat) {
+			if (isFont(file)) {
+				copyFile(file);
+			}
+		});
 
-    finder.on('file', function(file, stat) {
-      if (path.basename(path.dirname(file)) === 'fonts') copyFile(file);
-    });
+		function isFont(file) {
+			var ext = path.extname(file);
+			return ['.eot', '.svg', '.ttf', '.woff', '.woff2'].indexOf(ext) >= 0;
+		}
 
-    finder.on('end', function() {
-      callback(null);
-      return;
-    });
+		finder.on('end', function () {
+			callback(null);
+			return;
+		});
 
-    function copyFile(file) {
-      fs.createReadStream(file).pipe(fs.createWriteStream(path.join(appRoot, _this.dest, 'fonts', path.basename(file))));
-    }
-  }
+		function copyFile(file) {
+			fs.createReadStream(file).pipe(fs.createWriteStream(path.join(appRoot, dest, path.basename(file))));
+		}
+	}
 };
